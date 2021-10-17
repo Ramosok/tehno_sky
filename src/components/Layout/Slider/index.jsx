a1a0fabdfbbb751f89c9a2c58439f6f7206a1488
Ramosok <Ramosok86@gmail.com>
2021-10-17 19:20:04 +0300

    first commit

---
+++ src/components/Layout/Slider/index.jsx
@@ -0,0 +1,13 @@
+// libraries
+import React from 'react';
+// styles
+import './slider.css';
+const slider = () => {
+    return (
+        <div className="slider">
+            <h1>slider</h1>
+        </div>
+    );
+};
+
+export default slider;
---
+++ src/components/PageOne/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//
+import './pageOne.css'
+const Pages = () => {
+    return (
+        <div className="container__page__one">
+            <h1>Страница 1</h1>
+        </div>
+    );
+};
+
+export default Pages;
